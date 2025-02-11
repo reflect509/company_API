19db957a443b489a94b83a5377620f59adc76ae0
reflect <User@404-23>
2025-02-11 17:01:16 +0500

    Desktop app, base components added

---
+++ Desktop app/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_app.Services
+{
+    public class ApiService : IApiService
+    {
+        private readonly HttpClient httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            httpClient.BaseAddress = new Uri("https://localhost:10020/api/v1/");
+        }
+
+
+    }
+}
--- Desktop app/Views/WorkerManagement.xaml.cs
+++ Desktop app/Views/WorkerManagement.xaml.cs
@@ -12,11 +12,11 @@ using System.Windows.Shapes;
 namespace Desktop_app
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WorkerManagement.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WorkerManagement : Window
     {
-        public MainWindow()
+        public WorkerManagement()
         {
             InitializeComponent();
         }
